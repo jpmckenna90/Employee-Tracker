10fa638f93c52cab7031716b285f620cac6997df
jpm <jpmckenna90@gmail.com>
2020-02-17 00:15:58 -0500

    Worked out logic to pull all departments from database and push to an array to be accessed within the app.

--- app.js
+++ app.js
@@ -2,13 +2,15 @@ const inquirer = require("inquirer");
 const mysql = require("mysql");
 
 // List of possible initial choices
-const choices = ["View all employees", 
-                 "View all employees by department", 
-                 "View all employees by Manager",
-                 "Add Employee",
-                 "Remove Employee",
-                 "Update Employee", 
-                 "Update Employee Manager"];
+const choices = [
+  "View all employees",
+  "View all employees by department",
+  "View all employees by Manager",
+  "Add Employee",
+  "Remove Employee",
+  "Update Employee",
+  "Update Employee Manager"
+];
 
 // Create connection to database
 const connection = mysql.createConnection({
@@ -26,54 +28,121 @@ const connection = mysql.createConnection({
   database: "employeetracker_db"
 });
 
+connection.connect(function(err) {
+  if (err) throw err;
+  begin();
+});
 
-promptUser = () => {
-  inquirer.prompt([{ 
-    type: "list",
-    name: "action",
-    message: "What would you like to do?",
-    choices: choices
- }]).then(function({action}){
-   switch(action){
-     case choices[0]:
-       console.log(choices[0]);
-       return;
-      case choices[1]:
-        console.log(choices[1])
-        return;
-      case choices[2]:
-        console.log(choices[2]);
-        return;
-      case choices[3]:
-        console.log(choices[3]);
-        return;
-      case choices[4]:
-        console.log(choices[4]);
-        return;
-      case choices[5]:
-        console.log(choices[5]);
-        return;
-      case choices[6]:
-        console.log(choices[6]);
-        return;
-      default:
-        console.log("Please choose a valid option.");
-        promptUser();
-   }
-  //  console.log(action);
-    // return {action};
- });
+const departmentsArray = [];
+
+// Function to query database and gather all departments and add them to the departmentsArray. 
+getDepartments = () => {
+  connection.query("SELECT * FROM department", function(err, result) {
+    if (err) throw err;
+    result.forEach(department => {
+      if (!departmentsArray.includes(department.title)) {
+        departmentsArray.push(department.title);
+      }
+    });
+    console.log(departmentsArray);
+  });
 };
 
+getDepartments();
+
+promptUser = () => {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "action",
+        message: "What would you like to do?",
+        choices: choices
+      }
+    ])
+    .then(function({ action }) {
+      switch (action) {
+        case choices[0]:
+          viewAllEmployees();
+          return;
+        case choices[1]:
+          viewAllEmpByDept();
+          return;
+        case choices[2]:
+          viewAllEmpByManager();
+          return;
+        case choices[3]:
+          addEmployee();
+          return;
+        case choices[4]:
+          removeEmployee();
+          return;
+        case choices[5]:
+          updateEmployee();
+          return;
+        case choices[6]:
+          updateEmployeeManager();
+          return;
+        default:
+          console.log("Please choose a valid option.");
+          promptUser();
+      }
+    });
+};
 
 begin = () => {
   promptUser();
-  // const choice = promptUser();
-  // console.log(choice);
-  // switch(promptUser()){
-  //   case "View all employees":
-  //     console.log('yep');
-  // }
-}
+};
 
-begin();
+viewAllEmployees = () => {
+  connection.query("SELECT * FROM employee", function(err, res) {
+    if (err) throw err;
+    res.forEach(employee => {
+      console.log(employee.first_name + " " + employee.last_name);
+    });
+    connection.end();
+  });
+  proceed("viewAll");
+};
+
+viewAllEmpByDept = () => {
+  inquirer
+    .prompt({
+      type: "list",
+      name: "action",
+      message: "Please choose a department.",
+      choices: departmentsArray
+    })
+    .then(function() {
+      console.log("hello");
+      // res.forEach(employee => {
+      //   console.log(
+      //     employee.first_name + " " + employee.last_name + " , " + department
+      //   );
+      // });
+    });
+  connection.end();
+};
+
+proceed = fromState => {
+  switch (fromState) {
+    case "viewAll":
+      inquirer
+        .prompt({
+          type: "list",
+          name: "action",
+          question: "What would you like to do?",
+          choices: ["Main Menu", "Exit"]
+        })
+        .then(function({ action }) {
+          switch (action) {
+            case "Main Menu":
+              begin();
+              return;
+            case "Exit":
+              console.log("Exiting...");
+              return;
+          }
+        });
+  }
+};
